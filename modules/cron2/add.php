86c135b400868a1df698edbc771eaeeffd60f175
hnxfirefly <m@s63.at>
2017-12-16 12:50:55 +0100

    Fix disable Cron in cronjob

--- modules/cron2/add.php
+++ modules/cron2/add.php
@@ -6,8 +6,9 @@ $mf = new masterform();
 
 $mf->AddField(t('Name'), 'name');
 $mf->AddField(t('Statement'), 'function');
-$mf->AddField(t('Aktiv'), 'active');
+$mf->AddField(t('Aktiv'), 'active','','',1);
 $mf->AddField(t('Type'), 'type');
 $mf->AddField(t('Ausführen täglich, um'), 'runat');
 
-$mf->SendForm('index.php?mod=cron2&action=add', 'cron', 'jobid', $_GET['jobid']);
+$mf->SendForm('index.php?mod=cron2&action=add', 'cron', 'jobid', $_GET['jobid'])
+?>
